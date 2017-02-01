eb0a9793b4d24e5fa3cba7bda20c857631227e17
Joseph <joseph.a.gauthier@gmail.com>
2017-01-31 22:55:34 -0600

    Significantly increased speed

--- src/AI/HW3/Evolver.java
+++ src/AI/HW3/Evolver.java
@@ -5,10 +5,9 @@ import java.util.Random;
 
 public class Evolver {
 
-	int populationSize = 50;
-	int tournSize = 10;
-	int numEvolutions = 20;
-	double mutationRate = 0.015;
+	int populationSize = 20;
+	double mutationRate = 0.2; //Slightly high mutation rate
+	double crossoverRate = 0.5;
 	ArrayList<Population> populations;
 	
 	public Evolver()
@@ -21,36 +20,40 @@ public class Evolver {
 	public Population evolvePopulation()
 	{
 		int numIterations = 0;
-		Population finalPopulation = new Population(populationSize, false);
-		while(numIterations < numEvolutions) {
-			System.out.println("Generation " + (numIterations+1));
-			Population newPopulation = new Population(populationSize, false);
-			
-			//Populate the new population with new children
-			System.out.println("\tPopulating the new generation...");
-			for(int i = 0; i < newPopulation.size(); i++) {
-				Individual i1 = getNewParent(populations.get(numIterations));
-				Individual i2 = getNewParent(populations.get(numIterations));
-				Individual newChild = crossover(i1, i2);
-				newPopulation.saveIndividual(i, newChild);
+		Population finalPopulation = new Population(populationSize, true);
+		try {
+			while(Controller.doBattleNoGui(new NeuralAgent(finalPopulation.getFittest().genes), new NeuralAgent(Game.bob)) != 1) {
+			//while(numIterations < numEvolutions) {
+				System.out.println("Generation " + (numIterations+1));
+				Population newPopulation = new Population(populationSize, false);
+				
+				//Populate the new population with new children, gotten by crossing 2 random (favored) parents
+				for(int i = 0; i < newPopulation.size(); i++) {
+					Individual i1 = getNewParent(populations.get(numIterations));
+					Individual i2 = getNewParent(populations.get(numIterations));
+					Individual newChild = crossover(i1, i2);
+					newPopulation.saveIndividual(i, newChild);
+				}
+				
+				//Slightly mutate the population
+				for(int i = 0; i < newPopulation.size(); i++) {
+					mutate(newPopulation.get(i));
+				}
+				finalPopulation = newPopulation;
+				populations.add(finalPopulation);
+				numIterations++;
 			}
-			
-			//Slightly mutate the population
-			System.out.println("\tMutating the population...");
-			for(int i = 0; i < newPopulation.size(); i++) {
-				mutate(newPopulation.get(i));
-			}
-			finalPopulation = newPopulation;
-			populations.add(finalPopulation);
-			numIterations++;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return finalPopulation;
 	}
 	
 	public Individual getNewParent(Population pop)
 	{
-		Population tournament = new Population(tournSize, false);
-        for (int i = 0; i < tournSize; i++) {
+		Population tournament = new Population(5, false);
+        for (int i = 0; i < 5; i++) {
             int randomId = (int) (Math.random() * pop.size());
             tournament.saveIndividual(i, pop.get(randomId));
         }
@@ -61,7 +64,7 @@ public class Evolver {
 	public Individual crossover(Individual i1, Individual i2) {
         Individual newSol = new Individual();
         for (int i = 0; i < i1.size(); i++) {
-            if (Math.random() <= 0.5) {
+            if (Math.random() <= crossoverRate) {
                 newSol.setGene(i, i1.getGene(i));
             } else {
                 newSol.setGene(i, i2.getGene(i));
