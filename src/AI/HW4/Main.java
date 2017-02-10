f09a788ed59278eb9ec7dece71850e7a0a7bd222
Joseph Gauthier <jagauthi@uark.edu>
2017-02-10 08:18:01 -0600

    Fixed HW4, now the AI can win at TicTacToe

--- src/AI/HW4/Main.java
+++ src/AI/HW4/Main.java
@@ -68,6 +68,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
+		int bestScore = (1 == 1) ? 1 : 2;
 		new Main();
     }
 
--- src/AI/HW4/MiniMaxAI.java
+++ src/AI/HW4/MiniMaxAI.java
@@ -78,29 +78,29 @@ public class MiniMaxAI {
 	public int checkScores(int player)
 	{
 		int score = 0;
-		score += evaluateLine(0, 0, 1, 0, 2, 0, player);
-		score += evaluateLine(0, 1, 1, 1, 2, 1, player);
-		score += evaluateLine(0, 2, 1, 2, 2, 2, player);
-		score += evaluateLine(0, 0, 0, 1, 0, 2, player);
-		score += evaluateLine(1, 0, 1, 1, 1, 2, player);
-		score += evaluateLine(2, 0, 2, 1, 2, 2, player);
-		score += evaluateLine(0, 0, 1, 1, 2, 2, player);
-		score += evaluateLine(2, 0, 1, 1, 0, 2, player);
+		score += getScore(0, 0, 0, 1, 0, 2, player);
+		score += getScore(1, 0, 1, 1, 1, 2, player);
+		score += getScore(2, 0, 2, 1, 2, 2, player);
+		score += getScore(0, 0, 1, 0, 2, 0, player);
+		score += getScore(0, 1, 1, 1, 2, 1, player);
+		score += getScore(0, 2, 1, 2, 2, 2, player);
+		score += getScore(0, 0, 1, 1, 2, 2, player);
+		score += getScore(2, 0, 1, 1, 0, 2, player);
 	    return score;
 	}
 	
-	private int evaluateLine(int col1, int row1, int col2, int row2, int col3, int row3, int player) {
+	private int getScore(int col1, int row1, int col2, int row2, int col3, int row3, int player) {
 	    int score = 0;
 	    int opponent = -player;
 	 
-		if (board.getBoard()[col1][row1] == player) {
+		if (board.getBoard()[col1][row1] == -1) {
 			score = 1;
 		} 
-		else if (board.getBoard()[col1][row1] == opponent) {
+		else if (board.getBoard()[col1][row1] == 1) {
 			score = -1;
 		}
 		 
-		if (board.getBoard()[col2][row2] == player) {
+		if (board.getBoard()[col2][row2] == -1) {
 			if (score == 1) { 
 				score = 10;
 			} 
@@ -111,7 +111,7 @@ public class MiniMaxAI {
 				score = 1;
 			}
 		} 
-		else if (board.getBoard()[col2][row2] == opponent) {
+		else if (board.getBoard()[col2][row2] == 1) {
 			if (score == -1) {
 				score = -10;
 			} 
@@ -123,7 +123,7 @@ public class MiniMaxAI {
 			}
 		}
 		 
-		if (board.getBoard()[col3][row3] == player) {
+		if (board.getBoard()[col3][row3] == -1) {
 			if (score > 0) { 
 				score *= 10;
 			} 
@@ -134,7 +134,7 @@ public class MiniMaxAI {
 				score = 1;
 			}
 		} 
-		else if (board.getBoard()[col3][row3] == opponent) {
+		else if (board.getBoard()[col3][row3] == 1) {
 			if (score < 0) { 
 				score *= 10;
 			} 
@@ -148,24 +148,24 @@ public class MiniMaxAI {
 		return score;
 	}
  
-	private String[] winningPatterns = {
+	private String[] winStrings = {
 		"111000000", "000111000", "000000111", "100100100", "010010010", "001001001", "100010001", "001010100" 
 	};
  
 	public boolean hasWon(int player) {
-		String pattern = "";
+		String boardString = "";
 		
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col < 3; col++) {
 				if (board.getBoard()[col][row] == player)
-					pattern += "1";
+					boardString += "1";
 				else
-					pattern += "0";
+					boardString += "0";
 			}
 		}
 		
-		for (String winningPattern : winningPatterns) {
-			if (pattern.equals(winningPattern)) 
+		for (String pattern : winStrings) {
+			if (boardString.equals(pattern)) 
 				return true;
 		}
 		return false;
