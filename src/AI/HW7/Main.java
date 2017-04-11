649859a864112f0682944feeafefb6d8f9837d03
Joseph Gauthier <jagauthi@uark.edu>
2017-04-11 14:40:30 -0500

    Small fixes

--- src/AI/HW7/Main.java
+++ src/AI/HW7/Main.java
@@ -13,16 +13,9 @@ public class Main {
 	String[] policy;
 	
 	public Main() {
-		
-		/*
-		 * Useful resource: 
-		 * http://people.revoledu.com/kardi/tutorial/ReinforcementLearning/Q-Learning-Algorithm.htm
-		 */
-		
 		Q = new double[rows*cols][rows*cols]; //20 wide, 10 high
 		R = new int[rows*cols][rows*cols];
 		policy = new String[rows*cols];
-		
 		alpha_k = 0.1;
 		gamma = 0.97;
 		Random random = new Random();
@@ -35,37 +28,50 @@ public class Main {
 		}
 
 		//Setting the goal for the top right corner:
-		R[19][19] = 100;
-		R[18][19] = 100;
-		R[39][19] = 100;
+		R[19][19] = 1000;
+		R[18][19] = 1000;
+		R[39][19] = 1000;
 		
 		//Setting the wall along the column x=10
 		R[9][10] = Integer.MIN_VALUE;
+		R[10][10] = Integer.MIN_VALUE;
 		R[11][10] = Integer.MIN_VALUE;
 		R[29][30] = Integer.MIN_VALUE;
+		R[30][30] = Integer.MIN_VALUE;
 		R[31][30] = Integer.MIN_VALUE;
 		R[49][50] = Integer.MIN_VALUE;
+		R[50][50] = Integer.MIN_VALUE;
 		R[51][50] = Integer.MIN_VALUE;
 		R[69][70] = Integer.MIN_VALUE;
+		R[70][70] = Integer.MIN_VALUE;
 		R[71][70] = Integer.MIN_VALUE;
+		R[90][70] = Integer.MIN_VALUE;
 		//Hole in the wall
 		//R[89][90] = Integer.MIN_VALUE;
 		//R[91][90] = Integer.MIN_VALUE;
 		//R[109][110] = Integer.MIN_VALUE;
 		//R[111][110] = Integer.MIN_VALUE;
+		R[110][130] = Integer.MIN_VALUE;
 		R[129][130] = Integer.MIN_VALUE;
+		R[130][130] = Integer.MIN_VALUE;
 		R[131][130] = Integer.MIN_VALUE;
 		R[149][150] = Integer.MIN_VALUE;
+		R[150][150] = Integer.MIN_VALUE;
 		R[151][150] = Integer.MIN_VALUE;
 		R[169][170] = Integer.MIN_VALUE;
+		R[170][170] = Integer.MIN_VALUE;
 		R[171][170] = Integer.MIN_VALUE;
 		R[189][190] = Integer.MIN_VALUE;
+		R[190][190] = Integer.MIN_VALUE;
 		R[191][190] = Integer.MIN_VALUE;
 
 		int goalState = 19;
-		int counter = 0;
-		while(counter < 10000) {
-			counter++;
+		int numIterations = 100;
+		for(int counter = 0; counter < numIterations; counter++) {
+			if(counter % (numIterations/10) == 0){
+				System.out.println("Iteration " + counter);
+				printPolicy();
+			}
 			int state = random.nextInt(rows*cols); //initial state
 			while(state != goalState) {
 				int[] possibleNextStates = getPossibleActions(state);
@@ -75,23 +81,15 @@ public class Main {
 				int r = R[state][nextState];
 				double value = q + alpha_k * (r + gamma*maxQ-q);
 				Q[state][nextState] = value;
-				
 				state = nextState;
 			}
 		}
-		
-		getPolicies();
-		
-		for(int i = 0; i < rows*cols; i++) {
-			System.out.print(policy[i] + " ");
-			if(i%20 == 19)
-				System.out.println();
-		}
+		System.out.println("Iteration " + numIterations);
+		printPolicy();
 	}
 	
 	public int[] getPossibleActions(int state) {
 		ArrayList<Integer> a = new ArrayList<Integer>();
-		
 		if(state > 19)
 			a.add(state - 20);
 		if(state < 180)
@@ -120,7 +118,7 @@ public class Main {
 		return max;
 	}
 	
-	public void getPolicies() {
+	public void printPolicy() {
 		for(int i = 0; i < rows*cols; i++) {
 			int[] possibleStates = getPossibleActions(i);
 			int nextState = i;
@@ -133,6 +131,12 @@ public class Main {
 			}
 			if(i % 20 == 10 && i != 90 && i != 110) {
 				policy[i] = "#";
+			}
+			else if(i == 19) {
+				policy[i] = "G";
+			}
+			else if(i == 180) {
+				policy[i] = "S";
 			}
 			else if(nextState == i - 1) {
 				policy[i] = "<";
@@ -149,6 +153,13 @@ public class Main {
 			else
 				policy[i] = "O";
 		}
+		
+		for(int i = 0; i < rows*cols; i++) {
+			System.out.print(policy[i] + " ");
+			if(i%20 == 19)
+				System.out.println();
+		}
+		System.out.println();
 	}
 	
 	public static void main(String[] args) throws Exception
