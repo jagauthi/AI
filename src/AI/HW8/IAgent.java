6a07f2be858014c535d244f37a19ad48818fe8d9
Joseph Gauthier <jagauthi@uark.edu>
2017-04-11 15:08:36 -0500

    Started HW8

---
+++ src/AI/HW8/IAgent.java
@@ -0,0 +1,10 @@
+package HW8;
+
+// The contents of this file are dedicated to the public domain.
+// (See http://creativecommons.org/publicdomain/zero/1.0/)
+
+interface IAgent
+{
+	void reset();
+	void update(Model m);
+}
---
+++ src/AI/HW8/JosephGauthier.java
@@ -0,0 +1,18 @@
+package HW8;
+
+public class JosephGauthier implements IAgent{
+	
+	public JosephGauthier() {
+		System.out.println("lullerz");
+	}
+
+	@Override
+	public void reset() {
+		
+	}
+
+	@Override
+	public void update(Model m) {
+		
+	}
+}
