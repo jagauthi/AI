4eb3dc6f56e2aa26db1ead01c3f25076f8564f4d
Joseph Gauthier <jagauthi@uark.edu>
2017-04-14 12:53:58 -0500

    Fixed a small thing

--- src/AI/HW8/Game.java
+++ src/AI/HW8/Game.java
@@ -6,12 +6,15 @@ class Game {
 
 	static void fullTournament() throws Exception {
 		ArrayList<IAgent> al = new ArrayList<IAgent>();
+		/*
 		al.add(new PrescientMoron());
 		al.add(new Mixed());
 		al.add(new Human());
 		al.add(new Blitz());
 		al.add(new SittingDuck());
 		al.add(new AggressivePack());
+		*/
+		al.add(new JosephGauthier());
 		al.add(new Winner2015a());
 		al.add(new Winner2015b());
 		al.add(new Winner2016a());
@@ -20,16 +23,8 @@ class Game {
 	}
 
 	public static void main(String[] args) throws Exception {
-		Controller.doBattle(new Winner2015a(), new Winner2016a());
-		//Controller.doBattle(new Mixed(), new Blitz());
-		//Controller.doBattle(new Mixed(), new AggressivePack());
-		//Controller.doBattle(new Blitz(), new Mixed());
-		//Controller.doBattle(new Human(), new SittingDuck());
-		//Controller.doBattle(new Mixed(), new SittingDuck());
-		//Controller.doBattle(new Human(), new Blitz());
-		//Controller.doBattle(new PrescientMoron(), new SittingDuck());
-		//Controller.doBattle(new PrescientMoron(), new Human());
-		//Controller.doBattle(new Human(), new PrescientMoron());
+		Controller.doBattle(new JosephGauthier(), new Winner2016a());
+		//System.out.println(Controller.doBattleNoGui(new JosephGauthier(), new Winner2016a()));
 		//fullTournament();
 	}
 }
